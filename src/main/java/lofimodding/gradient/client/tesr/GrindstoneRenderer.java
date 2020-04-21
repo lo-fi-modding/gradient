7848ab4660e90cddb9e99528dd47c72d0dea7c34
Corey Frenette <corey@narwhunderful.com>
2020-04-21 17:40:10 -0300

    Render item counts for grindstone

--- src/main/java/lofimodding/gradient/client/tesr/GrindstoneRenderer.java
+++ src/main/java/lofimodding/gradient/client/tesr/GrindstoneRenderer.java
@@ -4,7 +4,9 @@ import com.mojang.blaze3d.matrix.MatrixStack;
 import lofimodding.gradient.blocks.GrindstoneBlock;
 import lofimodding.gradient.tileentities.GrindstoneTile;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.Matrix4f;
 import net.minecraft.client.renderer.Vector3f;
 import net.minecraft.client.renderer.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
@@ -47,7 +49,10 @@ public class GrindstoneRenderer extends TileEntityRenderer<GrindstoneTile> {
       final ItemStack input = te.getInput();
 
       if(input.getCount() > 1) {
-        //TODO this.drawNameplate(te, Integer.toString(input.getCount()), -0.5d, -1.05d, -0.5d, 16);
+        matrixStack.push();
+        matrixStack.translate(0.0d, 0.5d, 0.0d);
+        this.renderName(Integer.toString(input.getCount()), matrixStack, buffer, combinedLight);
+        matrixStack.pop();
       }
 
       matrixStack.scale(0.5f, 0.5f, 0.5f);
@@ -80,7 +85,10 @@ public class GrindstoneRenderer extends TileEntityRenderer<GrindstoneTile> {
       final ItemStack output = te.getOutput();
 
       if(output.getCount() > 1) {
-        //TODO this.drawNameplate(te, Integer.toString(output.getCount()), -0.5d, -1.05d, -0.5d, 16);
+        matrixStack.push();
+        matrixStack.translate(0.0d, 0.5d, 0.0d);
+        this.renderName(Integer.toString(output.getCount()), matrixStack, buffer, combinedLight);
+        matrixStack.pop();
       }
 
       matrixStack.scale(0.5f, 0.5f, 0.5f);
@@ -88,5 +96,24 @@ public class GrindstoneRenderer extends TileEntityRenderer<GrindstoneTile> {
       Minecraft.getInstance().getItemRenderer().renderItem(te.getOutput(), ItemCameraTransforms.TransformType.GROUND, combinedLight, combinedOverlay, matrixStack, buffer);
       matrixStack.pop();
     }
+  }
+
+  protected void renderName(final String text, final MatrixStack matrixStack, final IRenderTypeBuffer buffer, final int combinedLight) {
+    final Minecraft mc = Minecraft.getInstance();
+
+    matrixStack.push();
+    matrixStack.rotate(mc.getRenderManager().getCameraOrientation());
+    matrixStack.scale(-0.025F, -0.025F, 0.025F);
+
+    final float opacity = Minecraft.getInstance().gameSettings.getTextBackgroundOpacity(0.25F);
+    final int background = (int)(opacity * 255.0F) << 24;
+    final FontRenderer font = mc.getRenderManager().getFontRenderer();
+    final float x = -font.getStringWidth(text) / 2;
+
+    final Matrix4f top = matrixStack.getLast().getMatrix();
+    font.renderString(text, x, 0.0f, 0x20ffffff, false, top, buffer, true, background, combinedLight);
+    font.renderString(text, x, 0.0f, 0xffffffff, false, top, buffer, false, 0, combinedLight);
+
+    matrixStack.pop();
   }
 }
