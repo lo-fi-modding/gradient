972ed64df7e7676ca4373027b6d32577ffa31457
Corey Frenette <corey@narwhunderful.com>
2020-05-17 17:43:44 -0300

    Add translation for fibre torches advancement (closes #49)

--- src/main/java/lofimodding/gradient/data/GradientDataGenerator.java
+++ src/main/java/lofimodding/gradient/data/GradientDataGenerator.java
@@ -1166,6 +1166,7 @@ public final class GradientDataGenerator {
       this.age1("mixing_basin", "Mix It Up", "Craft a mixing basin");
       this.age1("firepit", "Open This Pit Up", "Craft a fire pit");
       this.age1("fire_starter", "Fire", "Craft a fire starter and use it to light your fire pit");
+      this.age1("fibre_torch", "Let There Be a Little Light", "Craft a fibre torch and light it");
       this.age1("pelt", "Animal Pelt", "Kill an animal and collect its pelt");
       this.age1("bone_awl", "Awl Be Back", "Craft an awl for working with animal pelts");
       this.age1("waterskin", "It's Made of What Now?", "Craft a waterskin to transport water");
