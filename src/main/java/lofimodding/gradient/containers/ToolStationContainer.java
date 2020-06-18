58e6a559e13e05f5a8c644843b046041b74428c0
Corey Frenette <corey@narwhunderful.com>
2020-06-18 08:15:53 -0300

    Slots

--- src/main/java/lofimodding/gradient/containers/ToolStationContainer.java
+++ src/main/java/lofimodding/gradient/containers/ToolStationContainer.java
@@ -13,22 +13,22 @@ public class ToolStationContainer extends GradientContainer<ToolStationTile> {
 
     // Recipe input
     for(int slot = 0; slot < tile.getMergedRecipeInv().getSlots(); slot++) {
-      this.addSlot(new SlotItemHandler(tile.getMergedStorageInv(), slot, INV_SLOTS_X + slot % size * SLOT_X_SPACING, slot / size * SLOT_Y_SPACING));
+      this.addSlot(new SlotItemHandler(tile.getMergedRecipeInv(), slot, INV_SLOTS_X + slot % size * SLOT_X_SPACING, slot / size * SLOT_Y_SPACING));
     }
 
     // Output
     for(int slot = 0; slot < tile.getMergedOutputInv().getSlots(); slot++) {
-      this.addSlot(new SlotItemHandler(tile.getMergedStorageInv(), slot, INV_SLOTS_X + (size + 1) * SLOT_X_SPACING, SLOT_Y_SPACING));
+      this.addSlot(new SlotItemHandler(tile.getMergedOutputInv(), slot, INV_SLOTS_X + (size + 1) * SLOT_X_SPACING, SLOT_Y_SPACING));
     }
 
     // Tools
     for(int slot = 0; slot < tile.getMergedToolsInv().getSlots(); slot++) {
-      this.addSlot(new SlotItemHandler(tile.getMergedStorageInv(), slot, INV_SLOTS_X + slot * SLOT_X_SPACING, (size + 1) * SLOT_Y_SPACING));
+      this.addSlot(new SlotItemHandler(tile.getMergedToolsInv(), slot, INV_SLOTS_X + slot * SLOT_X_SPACING, (int)((size + 0.5f) * SLOT_Y_SPACING)));
     }
 
     // Storage
     for(int slot = 0; slot < tile.getMergedStorageInv().getSlots(); slot++) {
-      this.addSlot(new SlotItemHandler(tile.getMergedStorageInv(), slot, INV_SLOTS_X + slot % 9 * SLOT_X_SPACING, (slot / 9 + size + 3) * SLOT_Y_SPACING));
+      this.addSlot(new SlotItemHandler(tile.getMergedStorageInv(), slot, INV_SLOTS_X + slot % 9 * SLOT_X_SPACING, (int)((slot / 9 + size + 1.5f) * SLOT_Y_SPACING)));
     }
 
     this.addPlayerSlots(playerInv);
